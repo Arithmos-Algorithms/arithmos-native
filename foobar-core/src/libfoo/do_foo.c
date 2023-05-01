955c2ca182c62797b8f1ccadd15023010853ba8a
pavl_g <60224159+Scrappers-glitch@users.noreply.github.com>
2023-05-01 16:09:42 +0300

    Initial commit

---
+++ foobar-core/src/libfoo/do_foo.c
@@ -0,0 +1,10 @@
+/**
+ * @brief Represents a compilation unit defining a function definition.
+ * @author pavl_g.
+ * @copyright Unlicensed license.
+ */
+#include <foobar.h>
+
+void do_foo() {
+	printf("Fetch me a tank of coffee!\n");
+}
---
+++ foobar-examples/src/hello_foobar.c
@@ -0,0 +1,14 @@
+/**
+ * @brief An example showing an example :-).
+ * @author pavl_g.
+ * @copyright Unlicensed license.
+ */
+#include <stdio.h>
+#include <foobar.h>
+
+int main() {
+    init_foo();
+    do_foo();
+    destroy_foo();
+    return 0;
+}

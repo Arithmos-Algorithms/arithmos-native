6e69e9209013c813a27417263c96c234e96a1332
pavl_g <scrappers.tm@gmail.com>
2023-05-16 11:16:16 +0300

    build: refactored STATIC_LIB_SOURCE to STATIC_LIB_DIR

--- helper-scripts/abstract/abstract-compile-examples.sh
+++ helper-scripts/abstract/abstract-compile-examples.sh
@@ -6,7 +6,7 @@ function compile() {
     local TOOLCHAIN_HEADERS=${3}
     local COMPILER_OPTIONS=${4}
     local TARGET=${5}
-    local STATIC_LIB_SOURCE=${6}
+    local STATIC_LIB_DIR=${6}
     local EXAMPLE=${7}
     local source_dir=${8}
     
@@ -16,7 +16,7 @@ function compile() {
           "-DCXX_COMPILER=${CXX_COMPILER}" \
           "-DTOOLCHAIN_HEADERS=${TOOLCHAIN_HEADERS}" \
           "-DCOMPILER_OPTIONS=${COMPILER_OPTIONS}" \
-          "-DSTATIC_LIB_SOURCE=${STATIC_LIB_SOURCE}" \
+          "-DSTATIC_LIB_DIR=${STATIC_LIB_DIR}" \
           "-DEXAMPLE=${EXAMPLE}" \
           -S . -B "./build/${TARGET}"
           

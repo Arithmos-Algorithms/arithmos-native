b1e4edf8ca0058f26fb045507a676b6323bba8da
pavl_g <scrappers.tm@gmail.com>
2023-05-16 10:56:43 +0300

    helper-scripts: attempt to fix example build

--- helper-scripts/project-impl/compile-examples.sh
+++ helper-scripts/project-impl/compile-examples.sh
@@ -6,10 +6,16 @@ source "./helper-scripts/project-impl/variables.sh"
 target_mcu=${1}
 example=${2}
 
+temp=$(pwd)
+cd "${examples_dir}"
+
 compile "${AVR_C_COMPILER}" \
         "${AVR_CXX_COMPILER}" \
         "${AVR_TOOLCHAIN_HEADERS}" \
         "-mmcu=${target_mcu} -O3" \
         "${target_mcu}" \
-        "${example}" \
-        "${examples_dir}"
+        "${example}" 
+        ""
+
+cd ${temp}
+        
